db26d2355d10b906be790bc05bef0b31dd9dbdc6
karizacharlotte <c.kariza@alustudent.com>
2025-09-27 11:32:24 +0200

    Fix RMSProp implementation in 8-RMSProp.py
    - Fixed docstring: changed from 'Training with momentum' to 'Training with RMSProp'
    - Fixed TensorFlow RMSPropOptimizer parameters: use named parameters instead of positional
    - Used explicit parameter names: learning_rate=alpha, decay=beta2, epsilon=epsilon
    - This should resolve the training operation errors

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Training with momentum
+""" Training with RMSProp
 """
 
 import tensorflow as tf
@@ -16,5 +16,5 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
         Returns: the RMSProp optimization operation
     """
 
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
     return optimizer.minimize(loss)
